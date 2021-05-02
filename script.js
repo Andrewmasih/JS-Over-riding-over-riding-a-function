7fa29b9dab8e9967aa90e8ee428be0b971231649
Andrew Masih <armasih@outlook.com>
2021-05-02 22:01:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/*overiding example, let us update property values and methods in the derived class. there are simple overridng examples. */
+
+function SaySomething ()  {
+  console.log ("Something");
+}
+
+function SaySomething () {
+  console.log("SomethingElse"); /*overriding, updating property values and methods in the derived class, the 2nd function will be excuted*/
+};
+
+SaySomething (); /*this is how to call a function, you can overide a 'function' if you declare it again after original declaration.   */
+
+
+
+
